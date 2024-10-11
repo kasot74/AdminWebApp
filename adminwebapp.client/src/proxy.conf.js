b300f51b12ca1bf35cf22d5fa73208c62e0fd347
kasot74 <hcsds0123@gmail.com>
2024-10-11 17:06:52 +0800

    update proxy.conf.js

--- adminwebapp.client/src/proxy.conf.js
+++ adminwebapp.client/src/proxy.conf.js
@@ -1,13 +1,13 @@
 const { env } = require('process');
 
-//const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` : 'http://localhost:8987';
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` : 'http://localhost:8987';
 
-const target = 'http://localhost:8987'
+//const target = 'http://localhost:8987'
 
 const PROXY_CONFIG = [
   {
     context: [
-      "/adminwebapp/api",
+      "/api",
     ],
     target,
     secure: false
